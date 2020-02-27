d9476d68fc3637cbcb25767b269c7f661138a1e0
nickherrig <neherrig@gmail.com>
2020-02-27 07:28:24 -0600

    Hoisted I/O for fetching post and error handling.

--- events_scraper.py
+++ events_scraper.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+import sys
 
 from bs4 import BeautifulSoup
 import requests
@@ -7,9 +8,21 @@ from pyalert import send_message
 
 
 url = 'https://ohospitality.com/events/'
-r = requests.get(url)
 
-html = BeautifulSoup(r.content, 'html.parser')
+def fetch_html(url):
+    try:
+        response = requests.get(url, timeout=5)
+    except requests.exceptions.ConnectionError as error:
+        print("Usually related to a network problem, (DNS failure, refused connection, etc)")
+        raise SystemExit(error)
+
+    if response.status_code != 200:
+        raise SystemExit('Non 200 recieved from url endpoint. Status code: ', response.status_code)
+    elif response.status_code == 200:
+        return response.content
+
+page_content = fetch_html(url)
+html = BeautifulSoup(page_content, 'html.parser')
 
 events = html.find_all('h3')
 page_links = html.find_all('a')
@@ -22,5 +35,5 @@ for link in page_links:
 for event, link in zip(events, purchase_links):
     alert_subject = 'New Event!'
     alert_body ='Event Name: {event} \n Purchase Tickets Here: {link}'.format(event=event.string, link=link) 
-    reciever = #TODO Replace with os env variable
+#TODO    reciever = Replace with os env variable
     send_message(reciever, alert_subject, alert_body)
