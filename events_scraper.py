4cd259a56c4246cb4961d46537d192ad40457cc3
nickherrig <neherrig@gmail.com>
2020-03-01 22:56:22 -0600

    Laptop swap, commit everything.

--- events_scraper.py
+++ events_scraper.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from pprint import pprint
 import sys
 
@@ -7,33 +8,39 @@ import requests
 from pyalert import send_message
 
 
+def parse_events(page_content):
+    html = BeautifulSoup(page_content, 'html.parser')    
+    event_names = html.find_all('h3')
+    page_links = html.find_all('a')
+    event_links = []
+    for link in page_links:
+        if link.string == 'Tickets + Menu':
+            event_links.append(link['href'])
+    Event = namedtuple('Event', 'name link')
+    events = []
+    for name, link in zip(event_names, page_links):
+       events.append(Event(name.string, link)) 
+    return events
+    
+
 url = 'https://ohospitality.com/events/'
 
-def fetch_html(url):
-    try:
-        response = requests.get(url, timeout=5)
-    except requests.exceptions.ConnectionError as error:
-        print("Usually related to a network problem, (DNS failure, refused connection, etc)")
-        raise SystemExit(error)
+try:
+    response = requests.get(url, timeout=5)
+except requests.exceptions.ConnectionError as error:
+    print("Usually related to a network problem, (DNS failure, refused connection, etc)")
+    raise SystemExit(error)
 
-    if response.status_code != 200:
-        raise SystemExit('Non 200 recieved from url endpoint. Status code: ', response.status_code)
-    elif response.status_code == 200:
-        return response.content
+if response.status_code != 200:
+    raise SystemExit('Non 200 recieved from url endpoint. Status code: ', response.status_code)
+elif response.status_code == 200:
+    events = parse_events(response.content)
 
-page_content = fetch_html(url)
-html = BeautifulSoup(page_content, 'html.parser')
+for event in events:
+    print(event)
 
-events = html.find_all('h3')
-page_links = html.find_all('a')
-purchase_links = []
-
-for link in page_links:
-    if link.string == 'Tickets + Menu':
-        purchase_links.append(link['href'])
-
-for event, link in zip(events, purchase_links):
-    alert_subject = 'New Event!'
-    alert_body ='Event Name: {event} \n Purchase Tickets Here: {link}'.format(event=event.string, link=link) 
-#TODO    reciever = Replace with os env variable
-    send_message(reciever, alert_subject, alert_body)
+#for event, link in zip(events, purchase_links):
+#    alert_subject = 'New Event!'
+#    alert_body ='Event Name: {event} \n Purchase Tickets Here: {link}'.format(event=event.string, link=link) 
+##TODO    reciever = Replace with os env variable
+#    send_message(reciever, alert_subject, alert_body)
